f9a80197b0dda9078ed92bf4b4388f065200da1e
mynzi1 <mynzi00@sookmyung.ac.kr>
2021-11-25 01:56:00 +0900

    Make UploadPage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,6 +8,8 @@ import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
 
+import UploadProductPage from "./views/UploadProductPage/UploadProductPage.js";
+
 //null   Anyone Can go inside
 //true   only logged in user can go inside
 //false  logged in user can't go inside
@@ -21,6 +23,7 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/product/upload" component={Auth(UploadProductPage, true)} />
         </Switch>
       </div>
       <Footer />
